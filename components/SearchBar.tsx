e913dbc666d8118f19c912230a39fd25cef28708
KartikeySriv <srikartikey11@gmail.com>
2025-07-19 12:23:55 +0530

    done with search page and search feature functioning

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,14 @@
 import { View, Text, Image, TextInput } from 'react-native';
 import { icons } from "@/constants/icons";
 
-interface Props{
-    placeholder:string;
-    onPress?:()=>void;
+interface Props {
+  placeholder: string;
+  value: string;
+  onChangeText: (text: string) => void;
+  onPress?: () => void;
 }
 
-const SearchBar = ({placeholder, onPress}:Props) => {
+const SearchBar = ({placeholder, value, onChangeText, onPress}:Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -16,10 +18,10 @@ const SearchBar = ({placeholder, onPress}:Props) => {
         tintColor="#ab8bff"
       />
       <TextInput
+        value={value}
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className='flex-1 ml-2 text-white'
       />
